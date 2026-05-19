d87385ee1d38432cc0bef29a8572606baa8735c0
miying-1 <sharonshareer@gmail.com>
2026-05-19 16:24:52 +0800

    chore: remove unused graph view component

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -54,7 +54,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
